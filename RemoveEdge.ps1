c1e9a946b0c8305af43fe48e70c3cb511d593184
SafewayDoge <n99230115@gmail.com>
2025-01-25 18:59:16 -0500

    RemoveEdge.ps1

--- RemoveEdge.ps1
+++ RemoveEdge.ps1
@@ -1,4 +1,3 @@
-# If script isn't running as admin, restart with admin privileges
 if (([Security.Principal.WindowsIdentity]::GetCurrent()).Owner.Value -ne "S-1-5-32-544") {
     $terminal = "powershell"
     if (Get-Command wt -ErrorAction SilentlyContinue) { $terminal = "wt" }
@@ -6,7 +5,6 @@ if (([Security.Principal.WindowsIdentity]::GetCurrent()).Owner.Value -ne "S-1-5-
     exit
 }
 
-# Exit if Edge was not found
 if (!(Test-Path "C:\Program Files (x86)\Microsoft\Edge")){
     $option = Read-Host "Edge was not Found, Would you Like to Re-install it? (Y/n)"
     if ("$option" -ne "n"){
@@ -16,29 +14,20 @@ if (!(Test-Path "C:\Program Files (x86)\Microsoft\Edge")){
     pause; exit
 }
 
-# Enable & create system restore point
 Enable-ComputerRestore -Drive "$env:HOMEDRIVE" 
 Checkpoint-Computer -Description "Before RemoveEdge" -RestorePointType "MODIFY_SETTINGS"
 
-# Create registry file that tells Windows you have the old non-Chromium Edge browser (disables updates)
 Write-Host "Disabling Edge Updates"
 New-Item -Path "HKLM:\SOFTWARE\Microsoft\" -Name "EdgeUpdate" -Force
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\EdgeUpdate\" -Name "DoNotUpdateToEdgeWithChromium" -Type DWORD -Value 1 -Force
 
-# Stop Edge
 Get-Process msedge -ErrorAction SilentlyContinue | Stop-Process
 
-# Remove Edge shortcuts
 Write-Host "Removing Edge Shortcuts From: Desktop & Start Menu"
 Get-Item "$HOME\Desktop\*","C:\ProgramData\Microsoft\Windows\Start Menu\Programs\*" -Include "*Microsoft Edge*" | % { Remove-Item "$_" -Force -Verbose }
 
-# Remove Edge data
-Write-Host "Removing Edge Data"
 Get-Item "C:\Program Files (x86)\Microsoft\Edge\","$env:LOCALAPPDATA\Packages\Microsoft.MicrosoftEdge*" | % {Remove-Item "$_" -Recurse -Force -Verbose}
-
-# Ask to reset taskbar pins & restart Explorer to apply changes
-$option = Read-Host "Would you Like to Reset Taskbar Pins? (Y/n)"
-if ("$option" -ne "n"){
+    
     Remove-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband" -Name "Favorites" -ErrorAction SilentlyContinue
-}
+
 Get-Process explorer | Stop-Process
