84aae9c8f31a09bc5f58b71238f4888ea735773d
SafewayDoge <n99230115@gmail.com>
2025-01-25 19:06:12 -0500

    Update RemoveEdge.ps1

--- RemoveEdge.ps1
+++ RemoveEdge.ps1
@@ -1,44 +1,9 @@
-# If script isn't running as admin, restart with admin privileges
-if (([Security.Principal.WindowsIdentity]::GetCurrent()).Owner.Value -ne "S-1-5-32-544") {
-    $terminal = "powershell"
-    if (Get-Command wt -ErrorAction SilentlyContinue) { $terminal = "wt" }
-        Start-Process wt -Verb RunAs "PowerShell -ExecutionPolicy Bypass -File `"$PSCommandPath`""
-    exit
-}
-
-# Exit if Edge was not found
-if (!(Test-Path "C:\Program Files (x86)\Microsoft\Edge")){
-    $option = Read-Host "Edge was not Found, Would you Like to Re-install it? (Y/n)"
-    if ("$option" -ne "n"){
-        winget install -s winget "Microsoft Edge"
-    }
-    Write-Host "Exiting"
-    pause; exit
-}
-
-# Enable & create system restore point
-Enable-ComputerRestore -Drive "$env:HOMEDRIVE" 
-Checkpoint-Computer -Description "Before RemoveEdge" -RestorePointType "MODIFY_SETTINGS"
-
-# Create registry file that tells Windows you have the old non-Chromium Edge browser (disables updates)
-Write-Host "Disabling Edge Updates"
-New-Item -Path "HKLM:\SOFTWARE\Microsoft\" -Name "EdgeUpdate" -Force
-New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\EdgeUpdate\" -Name "DoNotUpdateToEdgeWithChromium" -Type DWORD -Value 1 -Force
-
-# Stop Edge
+if (([Security.Principal.WindowsIdentity]::GetCurrent()).Owner.Value -ne "S-1-5-32-544") { Start-Process wt -Verb RunAs "PowerShell -ExecutionPolicy Bypass -File `"$PSCommandPath`""; exit }
+if (!(Test-Path "C:\Program Files (x86)\Microsoft\Edge")) { exit }
+Enable-ComputerRestore -Drive "$env:HOMEDRIVE"; Checkpoint-Computer -Description "Before RemoveEdge" -RestorePointType "MODIFY_SETTINGS"
+New-Item -Path "HKLM:\SOFTWARE\Microsoft\EdgeUpdate" -Force | Out-Null; New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\EdgeUpdate" -Name "DoNotUpdateToEdgeWithChromium" -Type DWORD -Value 1 -Force
 Get-Process msedge -ErrorAction SilentlyContinue | Stop-Process
-
-# Remove Edge shortcuts
-Write-Host "Removing Edge Shortcuts From: Desktop & Start Menu"
-Get-Item "$HOME\Desktop\*","C:\ProgramData\Microsoft\Windows\Start Menu\Programs\*" -Include "*Microsoft Edge*" | % { Remove-Item "$_" -Force -Verbose }
-
-# Remove Edge data
-Write-Host "Removing Edge Data"
-Get-Item "C:\Program Files (x86)\Microsoft\Edge\","$env:LOCALAPPDATA\Packages\Microsoft.MicrosoftEdge*" | % {Remove-Item "$_" -Recurse -Force -Verbose}
-
-# Ask to reset taskbar pins & restart Explorer to apply changes
-$option = Read-Host "Would you Like to Reset Taskbar Pins? (Y/n)"
-if ("$option" -ne "n"){
-    Remove-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband" -Name "Favorites" -ErrorAction SilentlyContinue
-}
-Get-Process explorer | Stop-Process
+Get-Item "$HOME\Desktop\*", "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\*", "C:\Program Files (x86)\Microsoft\Edge\", "$env:LOCALAPPDATA\Packages\Microsoft.MicrosoftEdge*" | Remove-Item -Recurse -Force
+Remove-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband" -Name "Favorites" -ErrorAction SilentlyContinue
+Stop-Process -Name explorer -Force
+#this removes edge without any questions
